d6aa5d9cdebfe3d5233021f4a892a07d0a805798
sasha2462ed <103293297+sasha2462ed@users.noreply.github.com>
2025-03-18 22:09:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+let checkAmigo = document.querySelector('amigo');
+
+
+function agregarAmigo (){
+if(checkAmigo == null)
+{
+    alert(checkAmigo);
+}
+}
+
